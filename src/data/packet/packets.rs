49a2ef513758193e8e96c88c8a4eed03eed5e7a5
pwf <aaron.lisman@gmail.com>
2023-03-07 19:07:20 -0600

    get rid of this boilerplate

--- src/data/packet/packets.rs
+++ src/data/packet/packets.rs
@@ -46,3 +46,47 @@ impl OpenRGBWritable for RequestProtocolVersion {
             .await
     }
 }
+
+// #[impl_packet(RequestControllerCountBody)]
+// pub struct RequestControllerCount {
+//     header: Header,
+//     body: RequestControllerCountBody,
+// }
+
+// pub struct RequestControllerCountBody {}
+
+// #[async_trait]
+// impl OpenRGBReadable for RequestControllerCountBody {
+//     async fn read(
+//         stream: &mut impl OpenRGBReadableStream,
+//         protocol: u32,
+//     ) -> Result<Self, OpenRGBError> {
+//         // consume client protocol version
+//         stream.read_value::<u32>(protocol).await?;
+//         Ok(RequestControllerCountBody {})
+//     }
+// }
+
+// #[async_trait]
+// impl OpenRGBWritable for RequestControllerCount {
+//     fn size(&self, protocol: u32) -> usize {
+//         protocol.size(protocol)
+//     }
+
+//     async fn write(
+//         self,
+//         stream: &mut impl OpenRGBWritableStream,
+//         protocol: u32,
+//     ) -> Result<(), OpenRGBError> {
+//         // respond with our version
+//         println!("responding with protocol version");
+//         stream
+//             .write_packet(
+//                 protocol,
+//                 self.header.device_id,
+//                 PacketId::RequestControllerCount,
+//                 protocol,
+//             )
+//             .await
+//     }
+// }
